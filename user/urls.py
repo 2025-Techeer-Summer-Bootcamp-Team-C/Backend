cf11d7d2ad003a8b89bab5a23371f0c5b7bcdd68
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-07-10 22:27:03 +0900

    refactor: url 경로 수정

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SignUpAPI
 
 urlpatterns = [
-    path("signup/", SignUpAPI.as_view(), name="api-signup"),
+    path("signup", SignUpAPI.as_view(), name="api-signup"),
 ]
