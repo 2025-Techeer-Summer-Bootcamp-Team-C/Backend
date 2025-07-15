65c336f64a1657b88e533081321a13430002031f
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-07-15 11:55:07 +0900

    feat: 회원가입 코드 수정 및 fitting_result모델 생성

--- user/models.py
+++ user/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from django.conf import settings
 
 class User(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name="사용자 이름",
+        help_text="한글 포함 가능",
+    )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일")
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name="수정일")
     deleted_at = models.DateTimeField(null=True, blank=True, verbose_name="삭제일")
