d1ae2de6c90ad7516ac8c9799bfadcc1e89ff2b9
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-07-15 12:21:30 +0900

    refactor: url 경로 수정

--- product/urls.py
+++ product/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('products', ProductCreateView.as_view(), name='product-create'),
-    path('products/<int:product_id>/images', ProductImageUploadView.as_view(), name='product-images-upload'),
+    path('', ProductCreateView.as_view(), name='product-create'),
+    path('<int:product_id>/images', ProductImageUploadView.as_view(), name='product-images-upload'),
 ]
