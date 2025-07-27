cf33a057b59a87484126e6b8ed526a5b8ed0c83e
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-07-27 15:12:37 +0900

    feat: cpu 바운드, i/o 바운드 워커 따로 구현

--- config/celery.py
+++ config/celery.py
@@ -16,7 +16,7 @@ app.conf.task_routes = {
     "fitting.tasks.edit_bg_task":          {"queue": "io"},
     "fitting.tasks.save_to_s3_and_db":     {"queue": "io"},
     # CPU 작업
-    "fitting.tasks.resize":                {"queue": "cpu"},
+    "fitting.tasks.resize_image_task":     {"queue": "cpu"},
 }
 
 app.conf.task_queues = (
